abcf7f3c49865b9f2972bc21fd692d8209923b2f
Dhominivs <dhominivs@gmail.com>
2023-05-24 20:50:47 -0300

    arquivos de icone

--- js/criartask.js
+++ js/criartask.js
@@ -1,7 +1,46 @@
 
+// ------------------------- TESTA SE O JS ESTA OK --------------------
+document.addEventListener('DOMContentLoaded', function(){
+    console.log("Check?, Checked!");
+    mostrarErro();
+});
+
+// --------------------------------------------------------------------
+
+// ------------------------- CRIAR UM TEXTAREA ------------------------
 function criarTextarea() {
     var textarea = document.createElement("textarea");
     document.body.appendChild(textarea);
+    
 }
+
 var botao = document.getElementById("criarTextarea");
 botao.addEventListener("click", criarTextarea);
+
+// --------------------------------------------------------------------
+
+// -------------- FUNCTION PARA VERIFICAR SE HÁ TEXTO -----------------
+function textoValido(texto){
+    if(texto == null || texto == "" || texto.lenght < 1){
+        return false;
+    }
+    else{
+        return true;
+    }
+}
+
+// --------------------------------------------------------------------
+
+// -------------------- FUNCTION PARA MOSTRAR ERROS -------------------
+function mostrarErro(){
+    var html = "";
+    html += '<div class="alert alert-danger" role="alert">';
+    html += 'Porfavor insira alguma coisa';
+    html += '</div>';
+    
+    document.getElementsById('error').innerHTML = html;
+    mostrarErro();
+
+}
+
+// --------------------------------------------------------------------
